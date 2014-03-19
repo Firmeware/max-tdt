6ba072b16a837ea1d96ae2d7be2bc5ba8ea90ca2
BPanther <bpanther_ts@hotmail.com>
2014-03-19 06:53:13 +0100

    fix

--- tdt/cvs/cdk/make/busybox.mk
+++ tdt/cvs/cdk/make/busybox.mk
@@ -4,6 +4,13 @@
 $(DEPDIR)/busybox: bootstrap @DEPENDS_busybox@ $(buildprefix)/Patches/busybox.config$(if $(UFS912)$(UFS913)$(SPARK)$(SPARK7162),_nandwrite)
 	@PREPARE_busybox@
 	cd @DIR_busybox@ && \
+<<<<<<< HEAD
+=======
+		patch -p1 < ../Patches/busybox-1.22.1-ash.patch && \
+		patch -p1 < ../Patches/busybox-1.22.1-date.patch && \
+		patch -p1 < ../Patches/busybox-1.22.1-iplink.patch && \
+		patch -p1 < ../Patches/busybox-1.22.1-nc.patch && \
+>>>>>>> cf6429b... busybox patch added
 		$(INSTALL) -m644 $(lastword $^) .config && \
 		sed -i -e 's#^CONFIG_PREFIX.*#CONFIG_PREFIX="$(targetprefix)"#' .config
 	cd @DIR_busybox@ && \
