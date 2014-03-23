35585579e9a290562636280632602c2377a02af5
max10 <max_10@gmx.de>
2014-03-23 17:51:55 +0100

    Revert "version bump util-linux-2.24"
    This reverts commit 1d4a8636ecfa88f2a2861755ac92733a8456e8d7.

--- tdt/cvs/cdk/make/contrib-apps.mk
+++ tdt/cvs/cdk/make/contrib-apps.mk
@@ -170,7 +170,6 @@ $(DEPDIR)/utillinux: bootstrap libz ncurses-dev @DEPENDS_utillinux@
 			--disable-static \
 			--disable-rpath \
 			--disable-bash-completion \
-			--without-ncurses \
 			--disable-wall && \
 		$(MAKE) ARCH=sh4 && \
 		@INSTALL_utillinux@
